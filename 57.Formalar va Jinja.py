69eba786c8ab36e74d65c1bf3cd8af4d00a15a7c
Dilshod <studywell206@gmail.com>
2026-08-06 12:45:30 +0500

    Add files via upload

---
+++ 57.Formalar va Jinja.py
@@ -0,0 +1,6 @@
+# har bir forma faylida agar front-end chi o'zi yozmagan bo'lsa
+# forma yaratayotganda \
+# <form> yozilgan bolsa biz uni <form method="POST"> ga almashtiramiz
+# hamda ostida
+# {%csrf_token%}
+# qoshib qoyamiz
